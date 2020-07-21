8d2ed41a510354eadfeaa72b5dc95ce9835ae9ee
Sudipta Pahar <49182601+sudipta-pahar@users.noreply.github.com>
2020-07-21 18:36:39 +0530

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,7 @@
+function onClickMenu() {
+    document.getElementById("menu").classList.toggle("change");
+    document.getElementById("nav-menu").classList.toggle("change");
+    document.getElementById("menu-bg").classList.toggle("change-bg");
+    // body...
+}
+
